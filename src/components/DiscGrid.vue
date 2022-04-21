9b3a7cc891c50b6a6874127af73f1d7c6339cead
Ivztoz8 <ivan.toselli31@gmail.com>
2022-04-21 20:32:39 +0200

    Stp3: Exe complete

--- src/components/DiscGrid.vue
+++ src/components/DiscGrid.vue
@@ -1,12 +1,9 @@
 <template>
-    <div class="container py-5">
-        <div class="row row-cols-6 text-white text-center g-4">
-            <div class="text-center bg-card mx-3" v-for="(element, index) in dischi" :key="index">
-                <img :src="element.poster" alt="" class="img-fluid my-3">
-                <h3 class="my-3">{{element.title}}</h3>
-                <h4 class="text-white-50 my-3">{{element.author}}</h4>
-                <h4 class="text-white-50 my-3">{{element.year}}</h4>
-            </div>
+    <div class="container-fluid py-5">
+        <div class="row row-cols-5 text-white g-2">
+            <CardDisco v-for="(element, index) in dischi" 
+            :key="index" :poster="element.poster" :title="element.title" :author="element.author" :year="element.year"
+            />
         </div>
     </div>
   
@@ -15,9 +12,13 @@
 <script>
 
 import axios from 'axios'
+import CardDisco from './sons/CardDisco.vue'
 
 export default {
   name: 'DiscGrid',
+  components: {
+      CardDisco 
+  },
   data(){
       return{
           dischi: []
@@ -34,11 +35,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
-@import './style/index.scss';
-
-.bg-card{
-    background-color: $header-bg;
-}
 
 </style>
---
+++ src/components/sons/CardDisco.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="col text-center bg-card">
+        <img :src="poster" alt="" class="img-fluid my-5">
+        <h3 class="my-1">{{title}}</h3>
+        <h4 class="text-white-50 my-2">{{author}}</h4>
+        <h4 class="text-white-50 my-2">{{year}}</h4>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardDisco',
+  props:{
+      poster: String,
+      title: String,
+      author: String,
+      year: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../style/index.scss';
+
+.bg-card{
+    background-color: $header-bg;
+}
+
+</style>
